394429426ca57171e4e06200622ceb33c6955198
Waweru-arch <waweruyrn@gmail.com>
2026-05-11 22:29:10 +0300

    Removed duplicate file

--- withdraw.py
+++
@@ -1,5 +0,0 @@
-# Allow the user to enter a sequence of nonnegative
-# integers. The user ends the list with a negative
-# integer. At the end the sum of the nonnegative
-# numbers entered is displayed. The program prints
-# zero if the user provides no nonnegative numbers
